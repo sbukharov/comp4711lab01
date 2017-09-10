aff6f1aeceb17be85253665f2eb56a3e2c1f6462
sbukharov <sergey.bukharov@cluberudite.com>
2017-09-10 13:40:04 -0700

    Instantiated two Students on index page to test functionality of Student class.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include('Student.php');
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
@@ -6,5 +7,30 @@
  * and open the template in the editor.
  */
 
-echo 'php for u and me';
- 
+$students = array();
+
+$first = new Student();
+$first->surname = "Doe";
+$first->first_name = "John";
+$first->add_email('home','john@doe.com');
+$first->add_email('work','jdoe@mcdonalds.com');
+$first->add_grade(65);
+$first->add_grade(75);
+$first->add_grade(55);
+$students['j123'] = $first;
+
+$second = new Student();
+$second->surname = "Einstein";
+$second->first_name = "Albert";
+$second->add_email('home','albert@braniacs.com');
+$second->add_email('work1','a_einstein@bcit.ca');
+$second->add_email('work2','albert@physics.mit.edu');
+$second->add_grade(95);
+$second->add_grade(80);
+$second->add_grade(50);
+$students['a456'] = $second;
+
+foreach($students as $student)
+    echo $student->toString();
+
+?>
